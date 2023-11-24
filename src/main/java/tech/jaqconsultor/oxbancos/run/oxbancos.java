09ce0e1f248cb7bf9e0d8de675d133afdbe9a579
Jesus Aquino <jaqconsultor@gmail.com>
2023-11-23 22:52:37 -0400

    hh

--- src/main/java/tech/jaqconsultor/oxbancos/run/oxbancos.java
+++ src/main/java/tech/jaqconsultor/oxbancos/run/oxbancos.java
@@ -11,7 +11,7 @@ import org.openxava.util.*;
 public class oxbancos {
 
 	public static void main(String[] args) throws Exception {
-		DBServer.start("oxbancos-db"); // Para usar tu propia base de datos comenta esta línea y configura src/main/webapp/META-INF/context.xml
+		//DBServer.start("oxbancos-db"); // Para usar tu propia base de datos comenta esta línea y configura src/main/webapp/META-INF/context.xml
 		AppServer.run("oxbancos"); // Usa AppServer.run("") para funcionar en el contexto raíz
 	}
 
